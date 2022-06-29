180e7a639a0ea9fb8b3e9914cd352e76be6b4505
subash <subash_axios@availfinance.in>
2022-06-29 20:27:08 +0545

    bug fixed in accordion

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,11 @@
 import "./Sidebar.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Accordion from "react-bootstrap/Accordion";
+import { useState } from "react";
 
 export const Sidebar = (props) => {
+  const history = useNavigate();
+  const [hideList, setHideList] = useState(false);
   return (
     <>
       <div className="cl-sidebar">
@@ -13,31 +16,77 @@ export const Sidebar = (props) => {
           </Link>
 
           <Accordion.Item eventKey="0">
-            <Accordion.Header className="menu-item">
+            <Accordion.Header
+              className="menu-item"
+              onClick={(e) => {
+                setHideList(false);
+              }}
+            >
               <i className="fa fa-money icon"></i>
               <span>Load Details</span>
             </Accordion.Header>
-            <Accordion.Body>
-              <Link className="item" to="/add-item">
-                Add New
-              </Link>
-              <Link className="item" to="/all-list">
-                Add List
-              </Link>
-            </Accordion.Body>
+            {!hideList && (
+              <Accordion.Body>
+                <p
+                  onClick={() => {
+                    if (props.showSidebar === false) {
+                      setHideList(true);
+                    }
+                    history("/add-item");
+                  }}
+                  className="item"
+                  to="/add-item"
+                >
+                  Add New
+                </p>
+                <p
+                  onClick={() => {
+                    if (props.showSidebar === false) {
+                      setHideList(true);
+                    }
+                    history("/all-list");
+                  }}
+                  className="item"
+                  to="/all-list"
+                >
+                  Add List
+                </p>
+              </Accordion.Body>
+            )}
           </Accordion.Item>
           <Accordion.Item eventKey="1">
-            <Accordion.Header className="menu-item">
+            <Accordion.Header
+              className="menu-item"
+              onClick={() => setHideList(false)}
+            >
               <i className="fa fa-floppy-o icon"></i>
               <span>Savings</span>
             </Accordion.Header>
             <Accordion.Body>
-              <Link className="item" to="/add-item">
+              <p
+                onClick={() => {
+                  if (props.showSidebar === false) {
+                    setHideList(true);
+                  }
+                  history("/add-item");
+                }}
+                className="item"
+                to="/add-item"
+              >
                 Add Item
-              </Link>
-              <Link className="item" to="/all-list">
+              </p>
+              <p
+                onClick={() => {
+                  if (props.showSidebar === false) {
+                    setHideList(true);
+                  }
+                  history("/all-list");
+                }}
+                className="item"
+                to="/all-list"
+              >
                 Add List
-              </Link>
+              </p>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
